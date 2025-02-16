fbde7d7ee1815eda2d00d35f18c81158262707a5
jenzi02 <jencenhuertacarrasco91@gmail.com>
2025-02-16 03:13:23 +0000

    jencen

---
+++ proyecto/controller/report.py
@@ -0,0 +1,30 @@
+from config.app import *
+import pandas as pd
+
+# crear un reporte diferente
+def GenerateReportVentas(app:App):
+    conn=app.bd.getConection()
+    query="""
+       SELECT 
+            proveedor_id, 
+            nombre_comercial, 
+            contacto_comercial, 
+            correo_electronico, 
+            telefono, 
+            saldo_pendiente, 
+            fecha_ultima_compra
+        FROM 
+             PROVEEDORES
+        WHERE 
+            saldo_pendiente > 0
+        ORDER BY 
+            saldo_pendiente DESC;
+    """
+    df=pd.read_sql_query(query,conn)
+    path=f"/workspaces/workspacepy0125v2/proyecto/files/data-Huerta.csv"
+    df.to_csv(path)
+    sendMail(app,path)
+
+def sendMail(app:App,data):
+    # cambiar el asunto 
+    app.mail.send_email('from@example','Reporte','Reporte',data)
