3276e6347ed31b62050eb94d09b8cf444998fe86
jenzi02 <jencenhuertacarrasco91@gmail.com>
2025-02-16 22:54:58 +0000

    ñ

--- proyecto/main.py
+++ proyecto/main.py
@@ -1,6 +1,6 @@
 from controller.menu import *
 from config.app import *
 if __name__=="__main__":
-    app=App('./workspaces/Practica4-Python-Datux/proyecto/datux.db')
+    app=App('./proyecto/datux.db')
     menu(app)
     
