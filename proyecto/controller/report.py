643776101fa8eced8da45e4bc8fd65b49320c198
jenzi02 <jencenhuertacarrasco91@gmail.com>
2025-02-16 23:28:10 +0000

    o

--- proyecto/controller/report.py
+++ proyecto/controller/report.py
@@ -1,16 +1,10 @@
 from config.app import *
 import pandas as pd
 
-import os
-
-from datetime import datetime
-from config.app import App  # Asegúrate de importar la clase App
-
-def GenerateReportVentas(app: App):
-    conn = app.bd.getConection()
-    
-    # Consulta SQL para obtener el país con menos productos comprados
-    query = """
+# crear un reporte diferente
+def GenerateReportVentas(app:App):
+    conn=app.bd.getConection()
+    query="""
         SELECT 
             p.pais,
             SUM(v.quantity) AS total_vendido
@@ -21,31 +15,15 @@ def GenerateReportVentas(app: App):
         GROUP BY 
             p.pais
         ORDER BY 
-            total_vendido ASC  -- Ordenado de menor a mayor
-        LIMIT 1;  -- Solo el país con menos compras
+                total_vendido ASC
+        LIMIT 1;
     """
-    
-    df = pd.read_sql_query(query, conn)
+    df=pd.read_sql_query(query,conn)
+    fecha="08-02"
+    path=f"/workspaces/Practica4-Python-Datux/proyecto/files/data-Huerta1.csv"
+    df.to_csv(path)
+    sendMail(app,path)
 
-    # Formatear la fecha actual para el nombre del archivo
-    fecha = datetime.now().strftime("%Y-%m-%d")
-    folder_path = "/workspaces/Practica4-Python-Datux/proyecto/files/data.H.csv"
-    file_path = f"{folder_path}/reporte_menor_ventas_{fecha}.csv"
-
-    # Crear carpeta si no existe
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-
-    # Guardar reporte en CSV
-    df.to_csv(file_path, index=False)
-    
-    # Enviar el reporte por correo
-    sendMail(app, file_path)
-
-def sendMail(app: App, file_path):
-    app.mail.send_email(
-        'from@example.com', 
-        'Reporte de Ventas (País con Menos Compras)', 
-        'Adjunto el reporte del país con menos compras.', 
-        file_path
-    )
+def sendMail(app:App,data):
+    # cambiar el asunto 
+    app.mail.send_email('from@example.com','Reporte de Ventas', 'Adjunto el reporte de ventas actualizado.', data)
